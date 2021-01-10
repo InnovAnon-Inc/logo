43ba939bf8e43e7f022ae4de2333b420c3a847ae
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-01-10 16:48:13 -0600

    bugfix

---
+++ check.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+set -euvxo pipefail
+#(( UID ))
+(( ! $# ))
+# TODO comprehensive list of artifacts
+cd out            
+[ -f android-chrome-144x144.png ]
+[ -f android-chrome-192x192.png ]
+[ -f android-chrome-256x256.png ]
+[ -f android-chrome-36x36.png ]
+[ -f android-chrome-384x384.png ]
+[ -f android-chrome-48x48.png ]
+[ -f android-chrome-72x72.png ]
+[ -f android-chrome-96x96.png ]
+[ -f apple-touch-icon-114x114.png ]
+[ -f apple-touch-icon-120x120.png ]
+[ -f apple-touch-icon-144x144.png ]
+[ -f apple-touch-icon-152x152.png ]
+[ -f apple-touch-icon-180x180.png ]
+[ -f apple-touch-icon-57x57.png ]
+[ -f apple-touch-icon-60x60.png ]
+[ -f apple-touch-icon-72x72.png ]
+[ -f apple-touch-icon-76x76.png ]
+[ -f favicon-16x16.png ]
+[ -f favicon-194x194.png ]
+[ -f favicon-32x32.png ]
+[ -f favicon.ico ]
+[ -f mstile-144x144.png ]
+[ -f mstile-150x150.png ]
+[ -f mstile-310x150.png ]
+[ -f mstile-310x310.png ]
+[ -f mstile-70x70.png ]
+[ -f safari-pinned-tab.svg ]
+
